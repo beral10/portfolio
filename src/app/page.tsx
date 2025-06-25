25db50ba713969825f74ac653b90b2bbaa0e6fa3
Yeferson Bernal <y.beral@gmail.com>
2025-06-25 10:37:24 -0500

    Modificación de padding Y en Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HomeComponent from '../components/HomeComponent';
 
 export default function Home() {
 	return (
-		<section className='flex flex-col justify-center items-center mx-auto max-w-[1500px] h-full shadow-black shadow-2xl px-[20px] bg-[#143c4e] py-12 overflow-hidden'>
+		<section className='flex flex-col justify-center items-center mx-auto max-w-[1500px] h-full shadow-black shadow-2xl px-[20px] py-12 bg-[#143c4e] overflow-hidden'>
 			<HomeComponent />
 			<AboutMeComponent />
 			<div className='fixed bottom-0 max-w-[1500px] w-full h-[100px] bg-gradient-to-b from-transparent to-[#92386d79] pointer-events-none z-20 overflow-hidden'></div>
